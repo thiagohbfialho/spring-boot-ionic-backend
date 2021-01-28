bf4fb61fc82bda7f1c5e6238fa76de3b623aaaf2
Thiago Fialho <thiago.fialhoti@gmail.com>
2021-01-28 09:42:25 -0300

    Listing all Categoria with DTO

--- src/main/java/com/thiagofialho/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/thiagofialho/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.thiagofialho.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagofialho.cursomc.domain.Categoria;
+import com.thiagofialho.cursomc.dto.CategoriaDTO;
 import com.thiagofialho.cursomc.services.CategoriaService;
 
 @RestController
@@ -48,5 +51,11 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+		
 }
